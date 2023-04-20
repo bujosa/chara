2cfb587585a0ff03839915856bf0c30ffa8e02e5
bujosa <davidbujosa@gmail.com>
2023-04-20 19:33:49 -0400

    feat: refactor main and run

---
+++ src/run.rs
@@ -0,0 +1,20 @@
+use clap::Arg;
+
+pub fn run() {
+    let matches = clap::App::new("First App with Clap")
+        .version("1.0")
+        .author("Bujosa <davidbujosa@gmail.com>")
+        .about("This is a simple app with clap, for learning purposes")
+        .arg(
+            Arg::new("help")
+                .short('h')
+                .long("help")
+                .help("Shows this help message"),
+        );
+
+    let matches = matches.get_matches();
+
+    if matches.is_present("help") {
+        println!("Help message");
+    }
+}
