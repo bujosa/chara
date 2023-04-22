4dcbc6f08041e996001693c126f371b93b6f4102
bujosa <davidbujosa@gmail.com>
2023-04-22 03:14:15 -0400

    feat: add build command in run

--- src/run.rs
+++ src/run.rs
@@ -1,28 +1,44 @@
-use clap::{command, Parser};
+use clap::{command, Parser, Subcommand};
+
+use crate::cli::build::{self, BuildCommand};
 
 #[derive(Parser, Debug)]
 #[command(
     author = "bujosa",
     about = "This is a simple app with clap, for learning purposes",
     display_name = "chara",
-    disable_help_subcommand = false,
+    disable_help_subcommand = true,
     version
 )]
 struct Opts {
-    #[arg(short, long)]
+    #[arg(short, long, help = "Activate debug mode")]
     debug: bool,
-    #[arg(short, long)]
+
+    #[arg(short, long, help = "Realize build mode")]
     build: bool,
+
+    #[command(subcommand)]
+    command: Command,
 }
+
+#[derive(Subcommand, Debug)]
+#[command()]
+enum Command {
+    /// With build u can realize a build
+    #[command()]
+    Build(BuildCommand),
+}
+
+#[derive(Subcommand, Debug)]
+#[command()]
+pub enum NoSubCommand {}
 
 pub fn run() {
     let cli = Opts::parse();
 
-    let res = match cli {
-        Opts { debug: true, .. } => "Debug mode",
-        Opts { build: true, .. } => "Build mode",
-        _ => "No mode",
+    match cli.command {
+        Command::Build(cmd) => {
+            build::parse(cmd);
+        }
     };
-
-    println!("{}", res);
 }
