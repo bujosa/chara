d552f2027adc13154c899647e3b1c4b8ef45b47a
bujosa <davidbujosa@gmail.com>
2023-04-24 19:35:29 -0400

    test: add example using test in build

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -19,3 +19,19 @@ pub fn build() {
 pub fn no_mode() {
     println!("No mode");
 }
+
+// Add test for build
+#[test]
+fn test_no_mode() {
+    no_mode();
+
+    assert_eq!(true, true);
+}
+
+// Add test for build_local
+#[test]
+fn test_build_local() {
+    build_local(true);
+
+    assert_eq!(true, true);
+}
